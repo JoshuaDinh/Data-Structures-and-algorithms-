b0d0e18cf6a6b1dd06c35a652fc7cc2da034fc8a
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-16 10:41:28 -0700

    completed reverse string exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,40 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// Array Solution #1
+// function reverse(str) {
+//   const arr = str.split("");
+//   arr.reverse();
+//   return arr.join("");
+// }
+// Short syntax - ^
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// For of loop Solution #2
+// function reverse(str) {
+//   let reversed = "";
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
+
+// Reduce solution #3
+// function reverse(str) {
+//  return str.split("").reduce((reversed, character) => {
+//       return character + reversed
+//   }, "");
+// }
+
+// Solution #4 favorite
+function reverse(str) {
+  let reversed = "";
+  for (let i = 0; i < str.length; i++) {
+    reversed = str[i] + reversed;
+  }
+  return reversed;
+}
 
 module.exports = reverse;
