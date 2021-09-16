69bac9d85bc878fab858634c065eb26336518134
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-16 13:33:06 -0700

    completed maxchar exercise

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let max = 0;
+  let maxKey = "";
+  let obj = {};
+
+  for (let char of str) {
+    if (!obj[char]) {
+      obj[char] = 1;
+    } else obj[char]++;
+  }
+  for (let char in obj) {
+    if (obj[char] > max) {
+      max = obj[char];
+      maxKey = char;
+    }
+  }
+  return maxKey;
+}
 
 module.exports = maxChar;
