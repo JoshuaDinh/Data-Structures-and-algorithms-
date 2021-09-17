35bb2107b93761c9e1495ce2a0b8bf95cd30e08d
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-17 08:05:53 -0700

    updated

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -10,8 +10,17 @@
 
 function reverseInt(n) {
   // reverse the number - turn into array - reverse array - join back into string
-  const reversed = n.toString().split("").reverse().join("");
-  return parseInt(reversed) * Math.sign(n);
+  let reversedInt = "";
+  const num = n.toString().split("");
+  for (let i = 0; i < num.length; i++) {
+    reversedInt = num[i] + reversedInt;
+  }
+  return parseInt(reversedInt) * Math.sign(n);
 }
+// function reverseInt(n) {
+//   // reverse the number - turn into array - reverse array - join back into string
+//   const reversed = n.toString().split("").reverse().join("");
+//   return parseInt(reversed) * Math.sign(n);
+// }
 module.exports = reverseInt;
 clearInterval;
